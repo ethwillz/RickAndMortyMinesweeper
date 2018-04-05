df543eecb53c1f41f10a7ff1019bbf57d0712500
ethwillz <ewwilliams6@gmail.com>
2018-04-05 00:43:30 -0500

    Got most of the work for adding bombs completely, should be finished tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import portal from './images/portal.png';
+import plumbus from './images/plumbus.png';
 import './App.css';
 
 /*
-  EASY: 15% bombs 8x8
-  MED: 25% bombs 12x12
-  HARD: 40% bombs 15x15
-  EXTR: 60% bombs 20x20
+  EASY: 15% 8x8
+  MEDI: 25% 12x12
+  HARD: 40% 15x15
+  EXTR: 60% 20x20
 */
 
 class App extends Component {
@@ -36,58 +37,59 @@ class Board extends App {
         lim = .61;
         break;
     }
-    let imgStyle = {
+    let portalStyle = {
       width: 70 / size + 'vh',
       height: 70 / size + 'vh',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      position: 'absolute',
     }
-    let spaces = Array(size).fill(Array(size).fill(false));
+    let spaceStyle = {
+      height: 70 / size + 'vh',
+      width: 70 / size + 'vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      position: 'relative',
+    }
+    let bombSpaces = Array(size).fill(0).map(row => new Array(size).fill(false));
     for(let i = 0; i < size; i++){
       for(let j = 0; j < size; j++){
-        if(Math.random() < lim) spaces[i][j] = true;
+        if(Math.random() < lim) bombSpaces[i][j] = true;
       }
     }
+
+    let spaces = Array(size).fill(0).map(row => new Array(size).fill(false));
     for(let i = 0; i < size; i++){
       for(let j = 0; j < size; j++){
+        let bombsAround = this.checkForBombs(bombSpaces, size, i, j);
         spaces[i][j] = <Space
-          image={portal}
-          hasBomb={spaces[i][j]}
-          bombsTouching={this.checkForBombs(spaces, size, i, j)}
-          imgStyle={imgStyle}
-          h2Style={{display: 'none'}} />
+          img = {portal}
+          hasBomb = {bombSpaces[i][j]}
+          bombsTouching = {bombsAround}
+          spaceStyle = {spaceStyle}
+          portalStyle = {portalStyle}
+          h2Style = {{display: 'none'}}/>;
       }
     }
+
     this.state = {
       spaces
     };
-
   }
 
   checkForBombs(spaces, size, i, j){
       let bombsTouching = 0;
+
       if(i > 0 && spaces[i-1][j]) bombsTouching++;
-      else if(i === 0 && spaces[size-1][j]) bombsTouching++;
-
       if(i > 0 && j < size - 1 && spaces[i-1][j+1]) bombsTouching++;
-      if(i === 0 && j === size - 1 && spaces[size-1][0]) bombsTouching++;
-
       if(j < size - 1 && spaces[i][j+1]) bombsTouching++;
-      if(j === size - 1 && spaces[i][0]) bombsTouching++;
-
       if(i < size - 1 && j < size - 1 && spaces[i+1][j+1]) bombsTouching++;
-      if(i === size - 1 && j < size - 1 && spaces[0][0]) bombsTouching++;
-
       if(i < size - 1 && spaces[i+1][j]) bombsTouching++;
-      if(i === size - 1 && spaces[0][j]) bombsTouching++;
-
       if(i < size - 1 && j > 0 && spaces[i+1][j-1]) bombsTouching++;
-      if(i === size - 1 && j === 0 && spaces[0][size-1]) bombsTouching++;
-
       if(j > 0 && spaces[i][j-1]) bombsTouching++;
-      if(j === 0 && spaces[i][size - 1]) bombsTouching++;
-
       if(i > 0 && j > 0 && spaces[i-1][j-1]) bombsTouching++;
-      if(i === 0 && j === 0 && spaces[size-1][size-1]) bombsTouching++;
+
       return bombsTouching;
   }
 
@@ -115,27 +117,58 @@ class Space extends Component {
   constructor(props){
     super(props);
     this.state = {
-      image: props.image,
+      img: props.img,
       onClick: props.onClick,
-      bombs: props.bombs,
-      imgStyle: props.imgStyle,
+      hasBomb: props.hasBomb,
+      spaceStyle: props.spaceStyle,
+      portalStyle: props.portalStyle,
       h2Style: props.h2Style,
-      bombsTouching: 0,
+      bombsTouching: props.bombsTouching,
       updateBombs: function(numBombs){
         this.setState({bombsTouching: numBombs})
       }
     }
+
+    this.onClick = this.onClick.bind(this);
   }
 
   onClick(){
-    this.state.setState({imgStyle: {display: 'none'}, h2Style: {display: 'block'}}, this.update);
+    console.log(this.state.hasBomb);
+    if(this.state.hasBomb){
+      console.log("hello");
+      this.setState({
+        img: plumbus,
+        //h2Style: {display: 'none'},
+      });
+    }
+
+    if(this.state.bombsTouching === 0){
+      this.setState({
+        portalStyle: {display: 'none'},
+        //h2Style: {display: 'none'},
+      });
+    }
+
+    if(!this.state.hasBomb && this.state.bombsTouching !== 0){
+      this.setState({
+        portalStyle: {display: 'none'},
+        h2Style: {display: 'block'},
+      });
+    }
   }
 
   render(){
     return (
-      <div className='Space'>
-        <img src={this.state.image} alt='bs' onClick={this.onClick} style={this.state.imgStyle}></img>
-        <h2 style={{display: 'none'}}>{this.state.bombsTouching}</h2>
+      <div className='Space' style={this.state.spaceStyle}>
+        <img
+          src={this.state.img}
+          alt='bs'
+          onClick={this.onClick}
+          style={this.state.portalStyle}>
+        </img>
+        <h2 style={{display: 'hidden', positon: 'absolute'}}>
+          {this.state.bombsTouching}
+        </h2>
       </div>
     )
   }
