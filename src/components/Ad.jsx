1adeda4f2086906a76c9ad24872249b958911b0b
ethwillz <ewwilliams6@gmail.com>
2018-04-12 12:05:45 -0500

    Put AdSense in, site needs to be approved in order to get them to render

---
+++ src/components/Ad.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class Ad extends React.Component {
+  componentDidMount () {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }
+
+render () {
+    return (
+        <ins className='adsbygoogle'
+          style={{ display: 'block' }}
+          google-ad-client='ca-pub-5566797500264030'
+          enable-page-level-ads='true' />
+    );
+  }
+}
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Ad from './Ad';
 
 export default class Board extends React.Component{
 
   render(){
     return (
       <div>
+        <Ad />
         <table>
           <tbody>
             {this.props.spaces.map((row, i) => <tr key={i}>{row}</tr>)}
           </tbody>
         </table>
+        <Ad />
       </div>
     )
   }
