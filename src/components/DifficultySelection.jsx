2cd700fb0eae8e1a1c5f67e743199e6aa9731e75
ethwillz <ewwilliams6@gmail.com>
2018-04-16 13:20:07 -0500

    Took out testing difficulty

--- src/components/DifficultySelection.jsx
+++ src/components/DifficultySelection.jsx
@@ -23,7 +23,6 @@ export default class DifficultySelection extends React.Component{
             textAlign: 'center',
             width: '70vw'
           }} >
-          <h2 style={{cursor: 'pointer', flex: '1', fontSize: '250%'}} onClick={this.onClick}>Testing</h2>
           <h2 style={{cursor: 'pointer', flex: '1', fontSize: '250%'}} onClick={this.onClick}>Easy</h2>
           <h2 style={{cursor: 'pointer', flex: '1', fontSize: '250%'}} onClick={this.onClick}>Medium</h2>
           <h2 style={{cursor: 'pointer', flex: '1', fontSize: '250%'}} onClick={this.onClick}>Hard</h2>
